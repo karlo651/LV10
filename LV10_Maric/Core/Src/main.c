79092c7f345aafc8573e3618f820dadb1f500f75
Karlo <karlomaric11@gmail.com>
2025-01-14 12:30:12 +0100

    dodan blinky program

--- LV10_Maric/Core/Src/main.c
+++ LV10_Maric/Core/Src/main.c
@@ -94,6 +94,9 @@ int main(void)
   /* USER CODE BEGIN WHILE */
   while (1)
   {
+
+	  HAL_GPIO_TogglePin(GPIOA, GPIO_PIN_5);
+	  HAL_Delay(100);
     /* USER CODE END WHILE */
 
     /* USER CODE BEGIN 3 */
